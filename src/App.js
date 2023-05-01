e470e5dc90c6e3fda34087af2e0db8651f658667
Deep Patel <deepp9185@gmail.com>
2023-05-01 18:11:36 +0530

    completed books management-App

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useEffect, useContext } from "react";
+
+import BooksContext from "./context/books";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+
+function App() {
+  const { fetchBooks } = useContext(BooksContext);
+
+  useEffect(() => {
+    fetchBooks();
+  }, [fetchBooks]);
+
+  return (
+    <div className="app">
+      <h1>Reading List</h1>
+      <BookList />
+      <BookCreate />
+    </div>
+  );
+}
+
+export default App;
